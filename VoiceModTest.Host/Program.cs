e31aae83b203b91b1f926d738e461aeda43eb1ec
Nicky <nbonelloghio@hotmail.com>
2021-03-10 23:07:01 +0100

    refactor: implement messaging server and client logic
    This change adds server functionality to host multiple connections at the same time. Messages by any client are forwarded to any client on the same server

--- VoiceModTest.Host/Program.cs
+++ VoiceModTest.Host/Program.cs
@@ -5,66 +5,32 @@ using System.Net.NetworkInformation;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Fleck;
+using VoiceModTest.Host.services;
 
 namespace VoiceModTest.Host
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             //get port from args
             var port = 8181;
             var available = CheckIfPortAvailable(port);
 
             if (available)
             {
-                var server = new WebSocketServer($"ws://0.0.0.0:{port}");
-                server.Start(socket =>
-                {
-                    socket.OnOpen = () => Console.WriteLine("Open!");
-                    socket.OnClose = () => Console.WriteLine("Close!");
-                    socket.OnMessage = message => socket.Send(message);
-                });
+                new MessagingServer($"ws://0.0.0.0:{port}");
             }
 
-            var wscli = new ClientWebSocket();
-            var tokSrc = new CancellationTokenSource();
-            var task = wscli.ConnectAsync(new Uri($"ws://localhost:{port}"), tokSrc.Token);
-            task.Wait(); task.Dispose();
-            Console.WriteLine($"WebSocket to ws://localhost:{port} OPEN!");
-            Console.WriteLine("SubProtocol: " + wscli.SubProtocol ?? "");
+            var client = new MessageClient();
+            await client.Connect(new CancellationToken());
 
-            Console.WriteLine(@"Type ""exit<Enter>"" to quit... ");
-            for (var inp = Console.ReadLine(); inp != "exit"; inp = Console.ReadLine())
+            if (client.GetClientState() == WebSocketState.Open)
             {
-                var inputBytes = Encoding.UTF8.GetBytes(inp);
-                task = wscli.SendAsync(
-                            new ArraySegment<byte>(inputBytes),
-                            WebSocketMessageType.Text,
-                            true,
-                            tokSrc.Token
-                        );
-                task.Wait(); task.Dispose();
-
-                //wait for end of message
-                var buffer = new byte[2000];
-                var result = wscli.ReceiveAsync(buffer, tokSrc.Token).Result;
-
-
-                Console.WriteLine("**** sent msg");
-                var ans = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
-                Console.WriteLine($"Server replied: {result.ToString()}");
+                await client.Disconnect(new CancellationToken());
             }
-
-            if (wscli.State == WebSocketState.Open)
-            {
-                task = wscli.CloseAsync(WebSocketCloseStatus.NormalClosure, "", tokSrc.Token);
-                task.Wait(); task.Dispose();
-            }
-            tokSrc.Dispose();
             Console.WriteLine("WebSocket CLOSED");
         }
 
---
+++ VoiceModTest.Host/services/MessagingServer.cs
@@ -0,0 +1,74 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoiceModTest.Host.services
+{
+    public class MessagingServer
+    {
+        private List<IWebSocketConnection> _webConnections;
+        private FleckLog _logger;
+        private WebSocketServer _server;
+
+        //On first connection ask for the name and map the name -> ConnectionGuid in server
+        public MessagingServer(string serverURI)
+        {
+            _webConnections = new List<IWebSocketConnection>();
+            _logger = new FleckLog();
+            _server = new WebSocketServer(serverURI);
+            Start();
+        }
+
+        public void Start()
+        {
+            _server.Start(socket =>
+            {
+                socket.OnOpen = () => AddClientConnection(socket);
+
+                socket.OnClose = () => DeleteClientConnection(socket);
+
+                socket.OnMessage = message =>
+                {
+                    _webConnections.ForEach(s => s.Send(message));
+                };
+
+                socket.OnBinary = bytes =>
+                {
+                    Console.WriteLine($"Received binary message from: {socket.ConnectionInfo.Id}");
+                    _webConnections.ForEach(s => s.Send($"{socket.ConnectionInfo.Id} : {Encoding.UTF8.GetString(bytes)}"));
+                };
+
+                socket.OnError = exception =>
+                    Console.WriteLine($"OnError {exception.Message}");
+
+                //socket.OnPing = bytes =>
+                  //  socket.Send("Pong");
+
+               // socket.OnPong = bytes =>
+                 //   socket.Send("Ping");
+            });
+
+
+            Console.WriteLine("Server established. Awaiting connections...");
+        }
+
+        public void AddClientConnection(IWebSocketConnection socket)
+        {
+            Console.WriteLine($"New client connection: {socket.ConnectionInfo.Id}");
+            _webConnections.Add(socket);
+        }
+
+        //??
+        public void DeleteClientConnection(IWebSocketConnection socket)
+        {
+            Console.WriteLine($"Client disconnected: {socket.ConnectionInfo.Id}");
+            _webConnections.Remove(socket);
+        }
+
+        public void Shutdown()
+        {
+            _server.Dispose();
+        }
+    }
+}
