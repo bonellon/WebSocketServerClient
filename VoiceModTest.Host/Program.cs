2519aef8613dfa7954760e3fcc9494335ca4b367
Nicky <nbonelloghio@hotmail.com>
2021-03-12 00:18:02 +0100

    refactor: add logging and finalise dependency injection

--- VoiceModTest.Host/Program.cs
+++ VoiceModTest.Host/Program.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Net.WebSockets;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using VoiceModTest.Host.services;
 
 namespace VoiceModTest.Host
 {
-
+    /// <summary>
+    /// The application entry point
+    /// </summary>
     class Program
     {
         static async Task Main(string[] args)
@@ -20,40 +15,10 @@ namespace VoiceModTest.Host
 
             using var applicationScope = serviceProvider.CreateScope();
 
-            var logger = applicationScope.ServiceProvider.GetService<ILogger<Program>>();
+            var runner = applicationScope.ServiceProvider.GetService<Runner>();
 
-            //get port from args
-            var port = 8181;
-            var available = CheckIfPortAvailable(port);
-
-            if (available)
-            {
-                var serverUri = $"ws://0.0.0.0:{port}";
-                logger.LogInformation($"Initialising new Server at URI: {serverUri}");
-                new MessagingServer(applicationScope.ServiceProvider.GetService<ILogger<MessagingServer>>(), serverUri);
-            }
-
-            var client = new MessageClient();
-            var cancellationTokenSource = new CancellationTokenSource(5000);
-            await client.Connect(cancellationTokenSource.Token)
-                .ContinueWith((t) => cancellationTokenSource.Dispose());
-
-            if (client.GetClientState() == WebSocketState.Open)
-            {
-
-                cancellationTokenSource = new CancellationTokenSource(5000);
-                await client.Disconnect(cancellationTokenSource.Token)
-                    .ContinueWith((t) => cancellationTokenSource.Dispose()); ;
-            }
-            Console.WriteLine("WebSocket CLOSED");
+            await runner.RunAsync();
         }
 
-        private static bool CheckIfPortAvailable(int port)
-        {
-            var properties = IPGlobalProperties.GetIPGlobalProperties();
-            var endpoint = properties.GetActiveTcpListeners();
-            var ports = endpoint.Select(p => p.Port).ToList();
-            return !ports.Contains(port);
-        }
     }
 }
